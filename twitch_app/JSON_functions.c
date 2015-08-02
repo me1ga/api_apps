9476ea066a26ab52ef14f6b4d59fb84b4951f0d2
me1ga <ws_mega@live.ru>
2015-08-02 13:28:57 +0600

    Написана процедура RemoveHeaders

--- twitch_app/JSON_functions.c
+++ twitch_app/JSON_functions.c
@@ -1,4 +1,15 @@
-void RemoveHeaders(char * string){
-	//dg
+void RemoveHeaders(char * string, int len){
+	char *p, c = 123; //123 = "{"
+	int plen = 0, last_s = 0;
+	p = memchr(string, c, len);
+	plen = strlen(p);
+	for ( last_s = plen ; last_s > 1 ; last_s-- ){
+		if ( 125 == p[last_s] ) break; //125 = "}"
+	}
+	char buffer[2048];
+	memset(buffer, 0, sizeof(buffer));
+	memcpy(buffer, p, last_s);
+	memset(string, 0, len);
+	memcpy(string, buffer, strlen(buffer));
 }
 
